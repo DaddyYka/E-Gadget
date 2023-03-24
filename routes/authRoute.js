1bc54b3c0c2b76920cf6e16a2a5188e0820db5a0
DaddyYka <albinvb90s@gmail.com>
2023-03-24 18:35:54 +0530

    final host condition
    E-com Gadget HOme project

---
+++ routes/authRoute.js
@@ -0,0 +1,53 @@
+import express, { Router } from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//Forgot Password || POST
+router.post('/forgot-password', forgotPasswordController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected User route auth
+router.get('/user-auth', requireSignIn, (req,res)=>{
+  res.status(200).send({ok:true});
+});
+
+//protected Admin route auth
+router.get('/admin-auth', requireSignIn, isAdmin, (req,res)=>{
+  res.status(200).send({ok:true});
+});
+
+//profile update routes
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//Admin all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+//order status complete
+router.put("/order-status/:orderId", requireSignIn, isAdmin, orderStatusController);
+
+export default router; 
